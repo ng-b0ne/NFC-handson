f8dee4c44e429626a67c08a66cea1786f4b056c5
b0ne <ng.b0ner@gmail.com>
2012-12-05 22:54:27 +0900

    add NFC intent-filter

--- src/com/example/android/nfchandson/MainActivity.java
+++ src/com/example/android/nfchandson/MainActivity.java
@@ -4,11 +4,11 @@ import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.IntentFilter.MalformedMimeTypeException;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
 import android.nfc.tech.NfcF;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
@@ -17,7 +17,6 @@ public class MainActivity extends Activity {
     private PendingIntent mPendingIntent;
     private IntentFilter[] mFilters;
     private String[][] mTechLists;
-    
     
     TextView nfcActionView;
     TextView nfcIdView;
@@ -28,40 +27,25 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-
         nfcActionView = (TextView)findViewById(R.id.nfc_action_view);
         nfcIdView = (TextView)findViewById(R.id.nfc_id_textview);
         nfcTagView = (TextView)findViewById(R.id.nfc_tag_view);
-        
-        //resolveIntent(getIntent());
-    }
-    
-    public void setDefaultValue(String action){
-        mAdapter = NfcAdapter.getDefaultAdapter(this);
 
-        // Create a generic PendingIntent that will be deliver to this activity. The NFC stack
-        // will fill in the intent with the details of the discovered tag before delivering to
-        // this activity.
+        mAdapter = NfcAdapter.getDefaultAdapter(this);
         mPendingIntent = PendingIntent.getActivity(this, 0,
                 new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
-
-        // Setup an intent filter for all MIME based dispatches
-        //IntentFilter ndef = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
-        IntentFilter ndef = new IntentFilter(action);
-        try {
-            ndef.addDataType("*/*");
-        } catch (MalformedMimeTypeException e) {
-            throw new RuntimeException("fail", e);
-        }
-        mFilters = new IntentFilter[] {
-                ndef,
-        };
-
-        // Setup a tech list for all NfcF tags
+        IntentFilter tag = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
+        mFilters = new IntentFilter[] { tag, };
         mTechLists = new String[][] { new String[] { NfcF.class.getName() } };
+        
+        //インテントを受け取ったら
+        if (getIntent().getAction() != null) {
+            resolveIntent(getIntent());
+        }
     }
     
     public void resolveIntent(Intent intent){
+        Log.v("TEST", "resolveIntent");
 
         String action = intent.getAction();
         if (isNFCAction(action)) {
@@ -106,24 +90,25 @@ public class MainActivity extends Activity {
     @Override
     public void onResume() {
         super.onResume();
-        //if (mAdapter != null) {
-            setDefaultValue(getIntent().getAction());
+        Log.v("TEST", "onResume");
+        if (mAdapter != null) {
             mAdapter.enableForegroundDispatch(this, mPendingIntent, mFilters,
                 mTechLists);
-        //}
+        }
     }
     
     @Override
     public void onPause() {
         super.onPause();
-        //if (mAdapter != null) mAdapter.disableForegroundDispatch(this);
-        if ( this.isFinishing() && mAdapter != null) {
+        Log.v("TEST", "onPause");
+        if (mAdapter != null) {
             mAdapter.disableForegroundDispatch(this);
         }
     }
     
     @Override
     public void onNewIntent(Intent intent) {
+        Log.v("TEST", "onNewIntent");
         setIntent(intent);
         resolveIntent(intent);
     }
